d3b176028aed05447787bbfb80c1cf303eeb2244
sangyaohui <176307420@qq.com>
2015-12-22 10:26:15 +0800

    重命名工程文件名，增加.gitignore的内容

--- AllHookInOne-v11.0/obj/local/armeabi/objs-debug/onehook/AppBehaviorCapturer/ApiInfo/ApiDeclaration.o.d
+++
@@ -1,8 +0,0 @@
-obj/local/armeabi/objs-debug/onehook/AppBehaviorCapturer/ApiInfo/ApiDeclaration.o: \
- jni/AppBehaviorCapturer/ApiInfo/ApiDeclaration.cpp \
- /home/sang/android/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream \
- jni/AppBehaviorCapturer/ApiInfo/ApiDeclaration.h
-
-/home/sang/android/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream:
-
-jni/AppBehaviorCapturer/ApiInfo/ApiDeclaration.h:
--- android_injection_control_program -时延测试版 V3.0/SwipeListViewExampleActivity/gen/com/fortysevendeg/android/swipelistview/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.fortysevendeg.android.swipelistview;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
