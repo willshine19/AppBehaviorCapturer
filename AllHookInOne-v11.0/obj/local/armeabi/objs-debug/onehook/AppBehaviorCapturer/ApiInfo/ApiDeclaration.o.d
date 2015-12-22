aa681d8674771b135b5d5541eee6fc8b0fd91491
sangyaohui <176307420@qq.com>
2015-12-22 16:39:59 +0800

    为merge做准备，添加.gitignore文件，修改工程名

--- AllHookInOne-v11.0/obj/local/armeabi/objs-debug/onehook/AppBehaviorCapturer/ApiInfo/ApiDeclaration.o.d
+++
@@ -1,8 +0,0 @@
-obj/local/armeabi/objs-debug/onehook/AppBehaviorCapturer/ApiInfo/ApiDeclaration.o: \
- jni/AppBehaviorCapturer/ApiInfo/ApiDeclaration.cpp \
- /home/zds/android/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream \
- jni/AppBehaviorCapturer/ApiInfo/ApiDeclaration.h
-
-/home/zds/android/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream:
-
-jni/AppBehaviorCapturer/ApiInfo/ApiDeclaration.h:
