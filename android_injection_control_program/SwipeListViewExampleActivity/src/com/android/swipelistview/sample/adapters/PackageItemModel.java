98c025178f773ff4045b099982b0c3f7f2d4398a
zhangdaishuai <769332496@qq.com>
2016-03-18 17:28:15 +0800

    syh

---
+++ android_injection_control_program/SwipeListViewExampleActivity/src/com/android/swipelistview/sample/adapters/PackageItemModel.java
@@ -0,0 +1,37 @@
+package com.android.swipelistview.sample.adapters;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+
+/**
+ * @author sang
+ * 单例模式
+ */
+public class PackageItemModel {
+
+	private ArrayList<PackageItem> list;
+	private Context context;
+	private static PackageItemModel singleton;
+	
+	private PackageItemModel() {
+		this.list = new ArrayList<PackageItem>();
+	}
+	
+	public static PackageItemModel getInstance() {
+		if (singleton == null) {
+			singleton = new PackageItemModel();
+		}
+		return singleton;
+	}
+	
+	public ArrayList<PackageItem> getList() {
+		return list;
+	}
+	
+	public void add(PackageItem item) {
+		list.add(item);
+	}
+	
+	
+}
